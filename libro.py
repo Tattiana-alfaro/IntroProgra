7c90fc10da83e3f0735c0402b6324d0e59e5621c
Tattiana-alfaro <78942371+Tattiana-alfaro@users.noreply.github.com>
2021-03-06 23:02:55 -0600

    Agregar funcion de buscar libro

--- libro.py
+++ libro.py
@@ -28,3 +28,65 @@ def imprirListaLibro():
     for Libro in cargarListaLibros():
         print(Libro.idLibro, " - ", Libro.nombre, " - ", Libro.genero, " - ", Libro.autor)
 
+def buscarLibro():
+
+    encontrado = False
+    texto = """
+    Por cual atributo le gustaria buscar
+    1. Codigo Libro
+    2. Nombre
+    3. Genero
+    4. Autor
+    """
+    print(texto)
+    opcion = input("Digite una opcion: ")
+    miLista = cargarListaLibros()
+    if (int(opcion) == 1):
+        attributo = input("Digite el codigo que desea buscar: ")
+        for Libro in miLista:
+            if str(attributo.capitalize()) in str(Libro.idLibro).capitalize():
+                encontrado = True
+                print("Se ha encontrado el siguiente registro: ")
+                print("CODIGO: ", Libro.idLibro) 
+                print("NOMBRE: ", Libro.nombre) 
+                print("GENERO: ", Libro.genero) 
+                print("AUTOR: ", Libro.autor) 
+        
+    if (int(opcion) == 2):
+        attributo = input("Digite el nombre que desea buscar: ")
+        for Libro in miLista:
+            if str(attributo.capitalize()) in str(Libro.nombre).capitalize():
+                encontrado = True
+                print("Se ha encontrado el siguiente registro: ")
+                print("CODIGO: ", Libro.idLibro) 
+                print("NOMBRE: ", Libro.nombre) 
+                print("GENERO: ", Libro.genero) 
+                print("AUTOR: ", Libro.autor) 
+        
+
+    if (int(opcion) == 3):
+        attributo = input("Digite el genero que desea buscar: ")
+        for Libro in miLista:
+            if str(attributo.capitalize()) in str(Libro.genero).capitalize():
+                encontrado = True
+                print("Se ha encontrado el siguiente registro: ")
+                print("CODIGO: ", Libro.idLibro) 
+                print("NOMBRE: ", Libro.nombre) 
+                print("GENERO: ", Libro.genero) 
+                print("AUTOR: ", Libro.autor) 
+
+    if (int(opcion) == 4):
+        attributo = input("Digite el autor que desea buscar: ")
+        for Libro in miLista:
+            if str(attributo.capitalize()) in str(Libro.autor).capitalize():
+                encontrado = True
+                print("Se ha encontrado el siguiente registro: ")
+                print("CODIGO: ", Libro.idLibro) 
+                print("NOMBRE: ", Libro.nombre) 
+                print("GENERO: ", Libro.genero) 
+                print("AUTOR: ", Libro.autor) 
+        
+    else:
+        if encontrado == False:
+            print("No se ha encontrado ninguna coincidencia")
+    miLista.clear()
